95eab0eba821df336ea91e1e89ad8e5a531c24bc
Sigureya <sigureya1208@gmail.com>
2024-07-06 21:35:29 +0900

    プリミティブ型を配置しなおし

---
+++ src/primitive/numbers.ts
@@ -0,0 +1,42 @@
+import { AnnotationBaseWidhDefault } from "../annotationPropety";
+import { ValueOf } from "../metatypes";
+
+export interface NumberArg extends AnnotationBaseWidhDefault {
+  min?: number;
+  max?: number;
+  digit?: number;
+  default: number;
+  type: "number";
+}
+export interface DataIndexArg<Name extends string>
+  extends AnnotationBaseWidhDefault {
+  type: Name;
+
+  default: number;
+}
+
+export interface Actor extends DataIndexArg<"actor"> {}
+export interface Switch extends DataIndexArg<"switch"> {}
+export interface Armor extends DataIndexArg<"armor"> {}
+export interface Skill extends DataIndexArg<"skill"> {}
+export interface Item extends DataIndexArg<"item"> {}
+export interface Weapon extends DataIndexArg<"weapon"> {}
+export interface Troop extends DataIndexArg<"troop"> {}
+export interface Class extends DataIndexArg<"class"> {}
+export interface State extends DataIndexArg<"state"> {}
+export interface CommonEvent extends DataIndexArg<"common_event"> {}
+type Primitive_Numbers = ValueOf<
+  [
+    NumberArg,
+    Actor,
+    Switch,
+    Armor,
+    Skill,
+    Item,
+    WakeLock,
+    Troop,
+    Class,
+    State,
+    CommonEvent
+  ]
+>;
---
+++ src/primitive/strings.ts
@@ -0,0 +1,22 @@
+import { AnnotationBaseWidhDefault } from "src/annotationPropety";
+
+export interface StringAnnotation extends AnnotationBaseWidhDefault {
+  type: "string";
+  default: string;
+}
+
+export interface MultilineString extends AnnotationBaseWidhDefault {
+  type: "multiline_string";
+  default: string;
+}
+
+export interface FilePathAnnotation extends AnnotationBaseWidhDefault {
+  type: "file";
+  default: string;
+  dir: string;
+}
+export interface ComboAnnotation extends AnnotationBaseWidhDefault {
+  default: string;
+  type: "combo";
+  options: string[];
+}
