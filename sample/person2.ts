429cf17bf1f67164a352ea68f7eeb23d846a9054
Sigureya <sigureya1208@gmail.com>
2024-07-09 01:49:28 +0900

    サンプル用のデータを更新

--- sample/person2.ts
+++ sample/person2.ts
@@ -2,9 +2,20 @@ export interface Person {
   name: string;
   age: number;
 }
-
 export interface Family {
   member: Person[];
   address: string;
   guest: Person;
+}
+export interface Road {
+  distance: number;
+}
+export interface Position {
+  x: number;
+  y: number;
+}
+export interface City {
+  fimilys: Family[];
+  road: Road[];
+  position: Position;
 }
