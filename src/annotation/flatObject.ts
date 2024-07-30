9724d61b4b3be29cddaaeecd506903f37525adf5
Sigureya <sigureya1208@gmail.com>
2024-07-30 09:22:55 +0900

    不要なコードを撤去

--- src/annotation/flatObject.ts
+++ src/annotation/flatObject.ts
@@ -5,11 +5,6 @@ type ReomoveArray<T> = T extends unknown[] ? T[number] : T;
 
 type ExtractObject<T> = Extract<ReomoveArray<T>, object>;
 
-type Pack<T> = (x: T) => void;
-
 export type FlatObject<T> = T extends object
   ? Struct<T> | FlatObject<ExtractObject<T[keyof T]>>
   : never;
-
-type c = FlatObject<City>;
-//type xx = c["structName"];
